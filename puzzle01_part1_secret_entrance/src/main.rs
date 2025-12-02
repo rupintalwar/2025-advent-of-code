e1b7a2f1184fd73cbe8b1159ad22727af0009800
rupintalwar <rupintalwar2@gmail.com>
2025-12-01 20:51:41 -0600

    name

--- puzzle01_part1_secret_entrance/src/main.rs
+++ puzzle01_part1_secret_entrance/src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 fn main() {
     let mut dial_location: i32 = 50;
     let mut count_zeros: u32 = 0;
-    let modulo: i32 = 100;
+    let modulus: i32 = 100;
     let file_path: &str = "input/input";
 
     if let Ok(lines) = read_lines(file_path) {
@@ -15,7 +15,7 @@ fn main() {
             let replace_l = replace_r.replace('L', "-");
             let movement: i32 = replace_l.parse().unwrap();
             
-            dial_location = (dial_location + movement).rem_euclid( modulo );
+            dial_location = (dial_location + movement).rem_euclid( modulus );
             if dial_location == 0
             {
                 count_zeros += 1;
--- puzzle01_part2_secret_entrance/src/main.rs
+++ puzzle01_part2_secret_entrance/src/main.rs
@@ -6,7 +6,7 @@ use num_traits::Euclid;
 fn main() {
     let mut dial_location: i32 = 50;
     let mut count_zeros: u32 = 0;
-    let modulo: i32 = 100;
+    let modulus: i32 = 100;
     let file_path: &str = "input/input";
 
     if let Ok(lines) = read_lines(file_path) {
@@ -16,7 +16,7 @@ fn main() {
             let replace_l = replace_r.replace('L', "-");
             let movement: i32 = replace_l.parse().unwrap();
              
-            let (div, rem): (i32, i32) = Euclid::div_rem_euclid( &(dial_location + movement), &modulo );
+            let (div, rem): (i32, i32) = Euclid::div_rem_euclid( &(dial_location + movement), &modulus );
             
             if movement < 0
             {
